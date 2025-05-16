8ea95481b769fd33dba7c2290402adb9e9476c0c
Sagiran Suresh <ssuresh14@myseneca.ca>
2025-05-16 13:24:39 -0400

    completed lab

---
+++ lab1d.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''This script demonstrates math operations using variables.'''
+
+x = 10
+y = 2
+z = 5
+
+# Perform the math using PEMDAS: 10 + 2 * 5 = 20
+result = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
