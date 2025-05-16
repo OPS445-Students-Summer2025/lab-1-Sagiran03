3021aa6b02ac5e79c571f1d852b37465cb2a3159
Sagiran Suresh <ssuresh14@myseneca.ca>
2025-05-16 13:21:17 -0400

    Added lab1b and lab1c

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''This script prints a question using a string object.'''
+
+# Assign a string to a variable
+name = 'Isaac'
+
+# Use the string in a print statement
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This script prints a sentence using string and integer objects.'''
+
+# Assign values
+name = 'Isaac'
+age = 72
+
+# Print the result
+print(name + ' is ' + str(age) + ' years old!')
